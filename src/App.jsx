47e79a7fd9e2d1b0f1a99e48654050ab1bfe73a3
Mariana <marianacm6@al.insper.edu.br>
2026-05-11 11:04:53 -0300

    Corrige App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,19 @@ function App() {
       .get("http://localhost:8000/api/notes/")
       .then((res) => setNotes(res.data));
   }, []); // [] → roda uma vez só
-
-  console.log(notes);
-
+  
   return (
     <>
       <AppBar />
-      
-      {notes.map((note) => (
-        <Note key={`note__${note.id}`} title={note.title}>
-          {note.content}
-        </Note>
-      ))}
+      <main className="container">
+        <div className="card-container">
+          {notes.map((note) => (
+            <Note key={`note__${note.id}`} title={note.title}>
+              {note.content}
+            </Note>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
